2d66c3148f4a1c707a9e1822cbfb66a60575de9d
jsh0620 <gcjsh0620@gmail.com>
2026-04-28 12:45:00 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -206,3 +206,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`memota-back running on port ${PORT}`));
 
 module.exports = app;
+
+const aiRouter = require('./routes/ai');
+app.use('/ai', aiRouter);
